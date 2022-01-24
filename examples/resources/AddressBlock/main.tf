75b3b07e3b73ef50f4ad1388c99021c38d723e95
Aleksei Chernevskii <aleksei_chernevskii@epam.com>
2022-01-24 18:26:23 +0300

    Created example *.tf files for the IPAM Address Block resource

---
+++ examples/resources/AddressBlock/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    b1ddi = {
+      version = "0.1"
+      source  = "github.com/infobloxopen/b1ddi"
+    }
+  }
+}
+
+resource "b1ddi_ip_space" "tf_example_space" {
+  name = "tf_example_space"
+}
+
+resource "b1ddi_address_block" "tf_example_address_block" {
+  address = "192.168.1.0"
+  cidr = 24
+  name = "tf_example_address_block"
+  space = b1ddi_ip_space.tf_example_space.id
+}
