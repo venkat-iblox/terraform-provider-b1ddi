2bb86748c540490ba782192215d1d9d346c00798
Aleksei Chernevskii <aleksei_chernevskii@epam.com>
2022-01-31 15:18:53 +0300

    Added example for the BloxOne DDI DNS Record
    * Added examples on how to create A, PTR, CNAME, and NS records.

---
+++ examples/resources/b1ddi_dns_record/resource.tf
@@ -0,0 +1,56 @@
+terraform {
+  required_providers {
+    b1ddi = {
+      version = "0.1"
+      source  = "github.com/infobloxopen/b1ddi"
+    }
+  }
+}
+
+resource "b1ddi_dns_view" "tf_example_dns_view" {
+  name = "example_tf_dns_view"
+}
+
+resource "b1ddi_dns_auth_zone" "tf_example_auth_zone" {
+  internal_secondaries {
+    host = "<On Prem Host Name>"
+  }
+  fqdn = "tf-example.com."
+  primary_type = "cloud"
+  view = b1ddi_dns_view.tf_example_dns_view.id
+}
+
+resource "b1ddi_dns_record" "a_record" {
+  zone = b1ddi_dns_auth_zone.tf_example_auth_zone.id
+  name_in_zone = "tf_example_a_record"
+  rdata = {
+    "address" = "192.168.1.15"
+  }
+  type = "A"
+}
+
+resource "b1ddi_dns_record" "ptr_record" {
+  zone = b1ddi_dns_auth_zone.tf_example_auth_zone.id
+  name_in_zone = "192.168.1.15"
+  rdata = {
+    "dname" = "tf_example_ptr_record"
+  }
+  type = "PTR"
+}
+
+resource "b1ddi_dns_record" "cname_record" {
+  zone = b1ddi_dns_auth_zone.tf_example_auth_zone.id
+  name_in_zone = "tf_example_cname_record"
+  rdata = {
+    "cname" = "canonical"
+  }
+  type = "CNAME"
+}
+
+resource "b1ddi_dns_record" "ns_record" {
+  zone = b1ddi_dns_auth_zone.tf_example_auth_zone.id
+  rdata = {
+    "dname" = "ns1.tf-example.com."
+  }
+  type = "NS"
+}
